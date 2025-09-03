bbc1bf183157b778fbbab46de62c262bf3ea0859
Naveen Kasi <kasivibes8888@gmail.com>
2025-09-03 17:07:25 +0530

    build: Configure project and add .env to gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { SEPOLIA_RPC_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
 };
